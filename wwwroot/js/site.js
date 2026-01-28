0053c0c24ad39cd89d076ae3921203e1cd44ca11
Sanjog Gurung <grgsandriyos@gmail.com>
2026-01-28 12:23:56 +0545

    Added Export Journals feature in the Journal Application.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -37,3 +37,20 @@ function renderLineChart(canvasId, labels, data) {
         }
     });
 }
+
+function generateSingleJournalPdf(entry) {
+    const doc = new jspdf.jsPDF();
+    doc.setFont("helvetica", "normal");
+
+    doc.setFontSize(16);
+    doc.text(entry.Title || "Untitled", 10, 20);
+
+    doc.setFontSize(10);
+    doc.text("Date: " + new Date(entry.EntryDate).toLocaleDateString(), 10, 30);
+
+    doc.setFontSize(12);
+    doc.text(entry.Content || "", 10, 40, { maxWidth: 180 });
+
+    doc.save((entry.Title || "JournalEntry") + ".pdf");
+}
+
